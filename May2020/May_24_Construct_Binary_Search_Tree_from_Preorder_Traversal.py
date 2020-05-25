8885ea601d826ca719ef5a9fd7685a0de3ce72a2
Suprit_Deepak <deepak.suprit@gmail.com>
2020-05-25 09:23:52 +0530

    Create May_24_Construct_Binary_Search_Tree_from_Preorder_Traversal.py

---
+++ May2020/May_24_Construct_Binary_Search_Tree_from_Preorder_Traversal.py
@@ -0,0 +1,46 @@
+"""
+Construct Binary Search Tree from Preorder Traversal
+Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+
+Example 1:
+
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+ 
+
+Constraints:
+
+1 <= preorder.length <= 100
+1 <= preorder[i] <= 10^8
+The values of preorder are distinct.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def bstFromPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        
+        #root = preorder[0]
+        root = TreeNode(preorder[0])
+        i = 1
+        while i< len(preorder) and preorder[i] <root.valgithu:
+            i +=1
+        root.left = self.bstFromPreorder(preorder[1:i])
+        root.right = self.bstFromPreorder(preorder[i:])
+        return root
